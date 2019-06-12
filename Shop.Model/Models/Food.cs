e8ccbe698037e57db9f21d44f4e34a700ece65fc
Chử Hoàng Anh <kukuanhanh@gmail.com>
2019-06-12 23:53:34 +0700

    new Food , order, orderdetail, table in model

---
+++ Shop.Model/Models/Food.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Shop.Model.Models
+{
+    [Table("Foods")]
+    public class Food
+    {
+        [Key]
+        public int FoodID { set; get; }
+        [Required]
+        [Column(TypeName ="nvarchar")]
+        [MaxLength(256)]
+        public string FoodName { set; get; }
+
+    }
+}
---
+++ Shop.Model/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shop.Model.Models
+{
+    [Table("Orders")]
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [MaxLength(256)]
+        public string Name { get; set; }
+
+        [Required]
+        public float Price { get; set; }
+
+        public Boolean Status { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }  
+
+
+    }
+}
